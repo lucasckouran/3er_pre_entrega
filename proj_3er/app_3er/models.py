5a822a530a0bcf508990b1b5edd71115cf84f8ac
Lucas Conte <lconte1892@gmail.com>
2023-06-07 18:24:51 -0300

    Creado 1 curso python de camada 4050

--- proj_3er/app_3er/models.py
+++ proj_3er/app_3er/models.py
@@ -5,6 +5,9 @@ class Curso(models.Model):
     nombre = models.CharField(max_length=40)
     camada = models.IntegerField()
 
+    def __str__(self):
+	    return f"Curso: {self.nombre} - Camada: {self.camada}"
+
 class Estudiante(models.Model):
 	nombre = models.CharField(max_length=40)
 	apellido = models.CharField(max_length=20)
