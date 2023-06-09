8edaf046ff710c5943c1f0355a0539ce6983c149
Lucas Conte <lconte1892@gmail.com>
2023-06-09 14:07:00 -0300

    cambios en la BD

--- proj_3er/app_3er/models.py
+++ proj_3er/app_3er/models.py
@@ -2,16 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Curso(models.Model):
-    nombre = models.CharField(max_length=40)
+    curso = models.CharField(max_length=40)
     camada = models.IntegerField()
 
     def __str__(self):
-	    return f"Curso: {self.nombre} - Camada: {self.camada}"
+	    return f"Curso: {self.curso} - Camada: {self.camada}"
 
 class Estudiante(models.Model):
 	nombre = models.CharField(max_length=40)
 	apellido = models.CharField(max_length=20)
 	email = models.EmailField(max_length=40)
+	def __str__(self):
+	    return f"Nombre: {self.nombre} - Apellido: {self.apellido} - email: {self.email}"
 
 class Profesor(models.Model):
 	nombre = models.CharField(max_length=40)
